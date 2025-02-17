11a3b39fd92297fbbe06911a1d73f3959a2fa983
Henrique Celso. <151757457+xXRICKIXx@users.noreply.github.com>
2025-02-17 11:36:33 -0300

    Update script.js2.js

--- script.js2.js
+++ script.js2.js
@@ -1,33 +1,4 @@
 function converteSegundosRelogio() {
-    let seg = prompt("digite um número")
-    seg = parseInt("digite os segundos")
-    let horas = parseInt(seg / 3600)
-    let minutos = parseInt((seg % 3600) / 60)
-    seg = segundos % 60
-    if (horas < 10) {
-        horas = "0" + horas
-    }
-    if (minutos < 10) {
-        minutos = "0" + minutos
-    }
-    if (seg < 10) {
-        seg = "0" + seg
-    }
-    console.log(horas + ":" + minutos + ":" + seg);
-}
-converteSegundosRelogio(seg)
-console.log("ihulllllll")
-converteSegundosRelogio()
-
-///    console.log("horas", horas);
-///   console.log("minutos", minutos);
-///    console.log("segundos", seg);
-
-
-
-
-
-function converteSegundosRelogio() {
     let horas = parseInt(seg / 3600)
     let minutos = parseInt((seg % 3600) / 60)
     seg = seg % 60
